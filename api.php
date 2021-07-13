d7e08f352066e2880aa12825ba83c01919fd341c
biskuizera19 <53500890+biskuizera19@users.noreply.github.com>
2021-07-13 13:56:35 -0300

    Update api.php

--- api.php
+++ api.php
@@ -12,13 +12,14 @@ $client = new Client();
 $datetime = date('Y-m-d H:i:s');
 /***********************************Banco************************************************/
 
-function api_login($usuario, $senha, $ip)
+
+function api_login($usuario, $senha, $ip, $segmento)
 {
     $client = new Client();
     $userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1';
     /*http://localhost:8085/api_laravel/api/api_cef/usuario=02943992321&senha=92943564&ip=168.121.65.214*/
     try {
-        $res = $client->request('GET', 'http://168.121.65.214:8090/api_laravel/api/api_cef/usuario=' . $usuario . '&senha=' . $senha . '&ip=' . $ip . '', [
+        $res = $client->request('GET', 'http://168.121.65.214:8090/api_laravel/api/api_cef/usuario=' . $usuario . '&senha=' . $senha . '&segmento=' . $segmento . '&ip=' . $ip . '', [
             'defaults' => [
                 'exceptions' => true
             ],
